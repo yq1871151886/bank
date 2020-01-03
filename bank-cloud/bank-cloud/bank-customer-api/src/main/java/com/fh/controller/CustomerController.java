6be20919ee856f8eb4e5d17710f22bb4b0126be2
yangqigong <1871151886@qq.com>
2020-01-03 09:23:08 +0800

    端口号设置

--- bank-cloud/bank-cloud/bank-customer-api/src/main/java/com/fh/controller/CustomerController.java
+++ bank-cloud/bank-cloud/bank-customer-api/src/main/java/com/fh/controller/CustomerController.java
@@ -3,15 +3,18 @@ package com.fh.controller;
 
 import com.fh.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("customer")
+@CrossOrigin(maxAge = 3600,origins = "http://localhost:8080",exposedHeaders = "NOLOGIN")
 public class CustomerController {
 
     @Autowired
     private CustomerService customerService;
+
 
 
 
