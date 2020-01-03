c586eabfa85c79171b5badaa8858e346b605e032
nuo-xi <1757663259@qq.com>
2020-01-03 22:16:55 +0800

    初始模块化配置-耿培

---
+++ bank-cloud/bank-cloud/bank-loan-api/src/main/java/com/fh/mapper/LoanMapper.java
@@ -0,0 +1,11 @@
+package com.fh.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.fh.beans.BankLoan;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface LoanMapper extends BaseMapper<BankLoan> {
+}
---
+++ bank-cloud/bank-cloud/bank-loan-api/src/main/java/com/fh/service/impl/LoanServiceImpl.java
@@ -0,0 +1,8 @@
+package com.fh.service.impl;
+
+import com.fh.service.LoanService;
+import org.springframework.stereotype.Service;
+
+@Service("loanService")
+public class LoanServiceImpl implements LoanService {
+}
