9531171f512374e8c97c3def51456e7dacb3b973
yangqigong <1871151886@qq.com>
2020-01-07 20:53:15 +0800

    开启consul，设置remit模块的consul

--- bank-cloud/bank-cloud/bank-remit-api/src/main/java/com/fh/BankRemitApiApplication.java
+++ bank-cloud/bank-cloud/bank-remit-api/src/main/java/com/fh/BankRemitApiApplication.java
@@ -2,8 +2,12 @@ package com.fh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableCircuitBreaker
+@EnableFeignClients
 public class BankRemitApiApplication {
 
     public static void main(String[] args) {
