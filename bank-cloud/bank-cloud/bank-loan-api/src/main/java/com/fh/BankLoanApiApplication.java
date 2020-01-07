ba803be6f72c345dda4d2b2347c9937f782c34cc
nuo-xi <1757663259@qq.com>
2020-01-07 16:59:29 +0800

    新增业务层代码-耿培

--- bank-cloud/bank-cloud/bank-loan-api/src/main/java/com/fh/BankLoanApiApplication.java
+++ bank-cloud/bank-cloud/bank-loan-api/src/main/java/com/fh/BankLoanApiApplication.java
@@ -2,8 +2,14 @@ package com.fh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
+@EnableRedisRepositories
 public class BankLoanApiApplication {
 
     public static void main(String[] args) {
--- bank-cloud/bank-cloud/bank-loan-api/src/main/java/com/fh/mapper/BankLoanMapper.java
+++ bank-cloud/bank-cloud/bank-loan-api/src/main/java/com/fh/mapper/BankLoanMapper.java
@@ -2,12 +2,20 @@ package com.fh.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.fh.beans.BankLoan;
+import com.fh.beans.BankLoanExample;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Mapper
 @Repository
 public interface BankLoanMapper extends BaseMapper<BankLoan> {
 
 
+    void updateByPrimaryKey(BankLoan bankLoan);
+
+    List<BankLoan> selectByExample(BankLoanExample bankLoanExample);
+
+    BankLoan selectByPrimaryKey(String transId);
 }
